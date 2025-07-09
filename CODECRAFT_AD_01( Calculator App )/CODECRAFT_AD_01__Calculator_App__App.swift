eba6570fc678ec319757c7ccd9cf724601808cbb
Shashika Theekshana <shashikatheekshana@icloud.com>
2025-07-09 12:54:39 +0530

    Initial Commit

---
+++ CODECRAFT_AD_01( Calculator App )/CODECRAFT_AD_01__Calculator_App__App.swift
@@ -0,0 +1,17 @@
+//
+//  CODECRAFT_AD_01__Calculator_App__App.swift
+//  CODECRAFT_AD_01( Calculator App )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+import SwiftUI
+
+@main
+struct CODECRAFT_AD_01__Calculator_App__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
