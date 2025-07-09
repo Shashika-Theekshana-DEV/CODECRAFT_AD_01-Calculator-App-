40bfc07a30d6462d94dd3de3c9563e0233c878a8
Shashika Theekshana <shashikatheekshana@icloud.com>
2025-07-09 19:43:15 +0530

    add CalculatorBrain to handle calculator logic and operations

---
+++ CODECRAFT_AD_01( Calculator App )/Model/CalculaterBrain.swift
@@ -0,0 +1,76 @@
+//
+//  CalculatorBrain.swift
+//  CODECRAFT_AD_01( Calculator App )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+
+import Foundation
+
+enum Operation {
+    case add, subtract, multiply, divide, none
+}
+
+struct CalculatorModel {
+    private var currentNumber: Double = 0
+    private var previousNumber: Double = 0
+    private var operation: Operation = .none
+    private var isPerformingOperation = false
+    
+    mutating func setOperand(_ operand: Double) {
+        currentNumber = operand
+    }
+    
+    mutating func performOperation(_ operation: Operation) -> Double? {
+        if !isPerformingOperation {
+            previousNumber = currentNumber
+            isPerformingOperation = true
+            self.operation = operation
+            return nil
+        } else {
+            switch self.operation {
+            case .add:
+                currentNumber = previousNumber + currentNumber
+            case .subtract:
+                currentNumber = previousNumber - currentNumber
+            case .multiply:
+                currentNumber = previousNumber * currentNumber
+            case .divide:
+                currentNumber = previousNumber / currentNumber
+            case .none:
+                break
+            }
+            self.operation = operation
+            return currentNumber
+        }
+    }
+    
+    mutating func calculate() -> Double {
+        switch operation {
+        case .add:
+            currentNumber = previousNumber + currentNumber
+        case .subtract:
+            currentNumber = previousNumber - currentNumber
+        case .multiply:
+            currentNumber = previousNumber * currentNumber
+        case .divide:
+            currentNumber = previousNumber / currentNumber
+        case .none:
+            break
+        }
+        isPerformingOperation = false
+        return currentNumber
+    }
+    
+    mutating func reset() {
+        currentNumber = 0
+        previousNumber = 0
+        operation = .none
+        isPerformingOperation = false
+    }
+    
+    func getCurrentNumber() -> Double {
+        return currentNumber
+    }
+}
